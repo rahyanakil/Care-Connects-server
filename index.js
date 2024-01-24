3a039716129b6aa152ad545fb08a8a4932448534
Rahyan Shamsi <129554871+rahyanshamsithroughltd@users.noreply.github.com>
2024-01-24 21:04:09 +0600

    modified code

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
-const nodemailer = require('nodemailer')
-const port = process.env.PORT || 5000
-const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 
 // middleware
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(morgan('dev'))
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan("dev"));
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zruwsn2.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -27,26 +27,28 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 
-// Validate jwt 
+// Validate jwt
 const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization
+  const authorization = req.headers.authorization;
   if (!authorization) {
     return res
       .status(401)
-      .send({ error: true, message: "Unauthorized Access" }) 
+      .send({ error: true, message: "Unauthorized Access" });
   }
-  const token = authorization.split(' ')[1]
+  const token = authorization.split(" ")[1];
   // token verify
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send({error: true, message: 'Unauthorized Access'})
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized Access" });
     }
-    req.decoded = decoded
-    next()
-  })
-}
+    req.decoded = decoded;
+    next();
+  });
+};
 
 //send mail function
 const sendMail = (emailData, emailAddress) => {
@@ -56,212 +58,278 @@ const sendMail = (emailData, emailAddress) => {
       user: process.env.EMAIL,
       pass: process.env.PASS,
     },
-  })
+  });
 
   const mailOptions = {
     from: process.env.EMAIL,
     to: emailAddress,
     subject: emailData.subject,
     html: `<p>${emailData?.message}</p>`,
-  }
+  };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log(error)
+      console.log(error);
     } else {
-      console.log("Email sent: " + info.response)
+      console.log("Email sent: " + info.response);
       // do something useful
     }
-  })
-}
+  });
+};
 
 async function run() {
   try {
-    const usersCollection = client.db('CareConnectsDb').collection('users')
-    const placesCollection = client.db('CareConnectsDb').collection('places')
-    const bookingsCollection = client.db('CareConnectsDb').collection('bookings')
+    const usersCollection = client.db("CareConnectsDb").collection("users");
+    const placesCollection = client.db("CareConnectsDb").collection("places");
+    const bookingsCollection = client
+      .db("CareConnectsDb")
+      .collection("bookings");
 
-    app.post('/create-payment-intent', verifyJWT, async (req,res) => {
-      const {price} = req.body
-      
-      if(price){
-        const amount = parseFloat(price)*100
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+
+      if (price) {
+        const amount = parseFloat(price) * 100;
         const paymentIntent = await stripe.paymentIntents.create({
           amount: amount,
           currency: "usd",
           payment_method_types: ["card"],
-        })
-        res.send({clientSecret: paymentIntent.client_secret})
+        });
+        res.send({ clientSecret: paymentIntent.client_secret });
       }
-    })
+    });
 
     // Generate jwt token
-    app.post('/jwt', async ( req, res )=> {
-      const email = req.body
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
       const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "7d",
-      })
-      res.send({token})
-    })
+      });
+      res.send({ token });
+    });
 
     // Svae user email & Role to database
-    app.put('/users/:email', async(req, res) => {
-      const email = req.params.email
-      const user = req.body
-      const query = {email: email}
-      const options = {upsert: true}
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
       const updateDoc = {
         $set: user,
-      }
-      const result = await usersCollection.updateOne(query, updateDoc, options)
-      res.send(result)
-    })
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
 
     // Get all places
-    app.get('/places', async(req, res) => {
-      const result = await placesCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/places", async (req, res) => {
+      const result = await placesCollection.find().toArray();
+      res.send(result);
+    });
 
-    //Delete place
-    app.delete('/places/:id', async(req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await placesCollection.deleteOne(query)
-      res.send(result)
-    })
+    //Delete doctors place
+    app.delete("/places/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await placesCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    // Get all places for host by email
-    app.get('/places/:email', verifyJWT, async(req, res) => {
-      const decodedEmail = req.decoded.email
-      const email = req.params.email
-      if(email !== decodedEmail){
+    // Get all doctors places for host by email
+    app.get("/places/:email", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.params.email;
+      if (email !== decodedEmail) {
         return res
           .status(403)
-          .send({ error: true, message: "Forbidden Access" })
+          .send({ error: true, message: "Forbidden Access" });
       }
-      const query = { 'host.email': email }
-      const place = await placesCollection.find(query).toArray()
-      res.send(place)
-    })
+      const query = { "host.email": email };
+      const place = await placesCollection.find(query).toArray();
+      res.send(place);
+    });
 
-    // Get a single place by id
-    app.get('/place/:id', async(req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const place = await placesCollection.findOne(query)
-      res.send(place)
-    })
+    // Get a single doctors place by id
+    app.get("/place/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const place = await placesCollection.findOne(query);
+      res.send(place);
+    });
 
-    // save a place in database
-    app.post('/places', async(req, res) => {
-      const place = req.body
-      const result = await placesCollection.insertOne(place)
-      res.send(result)
-    })
+    // save a doctor place in database
+    app.post("/places", async (req, res) => {
+      const place = req.body;
+      const result = await placesCollection.insertOne(place);
+      res.send(result);
+    });
 
-    // update a place in database
-    app.put('/places/:id', verifyJWT, async(req,res) => {
-      const place =req.body
-      console.log(place)
-      const filter = { _id: new ObjectId(req.params.id)}
-      const options = {upsert: true}
+    // update a doctor place in database
+    app.put("/places/:id", verifyJWT, async (req, res) => {
+      const place = req.body;
+      console.log(place);
+      const filter = { _id: new ObjectId(req.params.id) };
+      const options = { upsert: true };
       const updateDoc = {
         $set: place,
-      }
-      const result = await placesCollection.updateOne(filter, updateDoc, options)
-      res.send(result)
-    })
-   
-    //update place booking status
-    app.patch('/places/status/:id', async (req, res) => {
-      const id = req.params.id
-      const status = req.body.status
-      const query = { _id: new ObjectId(id) }
+      };
+      const result = await placesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    //update place appoinment booking status
+    app.patch("/places/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set: { 
+        $set: {
           booked: status,
-         },
+        },
+      };
+      const update = await placesCollection.updateOne(query, updateDoc);
+      res.send(update);
+    });
+
+    // Get bookings for patients
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
       }
-      const update = await placesCollection.updateOne(query, updateDoc)
-      res.send(update)
-    })
+      const query = { "guest.email": email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    // Get bookings for guest
-    app.get('/bookings', async (req, res) => {
-      const email = req.query.email
-      if(!email){
-        res.send([])
+    // Get bookings for doctor as host
+    app.get("/bookings/host", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
       }
-      const query = {'guest.email': email}
-      const result = await bookingsCollection.find(query).toArray()
-      res.send(result)
-    })
+      const query = { host: email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    // Get bookings for host
-    app.get('/bookings/host', async (req, res) => {
-      const email = req.query.email
-      if(!email){
-        res.send([])
+    //save a appoinment bookings to database
+    // app.post("/bookings", async (req, res) => {
+    //   const booking = req.body;
+    //   const result = await bookingsCollection.insertOne(booking);
+    //   // send confirmation email to patitnet  email account
+    //   sendMail(
+    //     {
+    //       subject: "Your Appoinment Booked Successful",
+    //       message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`,
+    //     },
+    //     booking?.guest?.email
+    //   );
+    //   // send confirmation email to doctors host email account
+    //   sendMail(
+    //     {
+    //       subject: "Your Appoinment booked Successfully!",
+    //       message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`,
+    //     },
+    //     booking?.host?.email
+    //   );
+    //   res.send(result);
+    // });
+
+    // ...
+
+    // Delete a booking by ID
+    app.delete("/bookings/:id", async (req, res) => {
+      const bookingId = req.params.id;
+
+      try {
+        const result = await bookingsCollection.deleteOne({
+          _id: new ObjectId(bookingId),
+        });
+
+        if (result.deletedCount === 1) {
+          res.status(200).json({ message: "Booking deleted successfully." });
+        } else {
+          res.status(404).json({ message: "Booking not found." });
+        }
+      } catch (error) {
+        console.error("Error deleting booking:", error);
+        res.status(500).json({ message: "Internal server error." });
       }
-      const query = { host: email }
-      const result = await bookingsCollection.find(query).toArray()
-      res.send(result)
-    })
+    });
 
-    // save a bookings to database
-    app.post('/bookings', async(req, res) => {
-      const booking = req.body
-      const result = await bookingsCollection.insertOne(booking)
-      // send confirmation email to guest email account
-      sendMail({
-        subject: 'Booking Successful',
-        message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`
-      }, 
-      booking?.guest?.email
-      )
-      // send confirmation email to host email account
-      sendMail({
-        subject: 'Your room got booked!',
-        message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`
-      }, 
-      booking?.host?.email
-      )
-      res.send(result)
-    })
+    //message sent by node mailer
 
-    //delete a booking
-    app.delete('/bookings/:id', async(req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await bookingsCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
 
-    // get user
-    app.get('/users/:email', async(req, res) => {
-      const email = req.params.email
-      const query = { email: email }
-      const user = await usersCollection.findOne(query)
-      res.send(user)
-    })
+      // Meeting Link
+      const meetingLink =
+        "https://calendly.com/rahyanakil89/appoinment-booking";
+
+      // Confirmation Email to Patient
+      const patientEmailContent = `
+        <h2>Thanks For Choosing Care Connect</h2>
+        <h4>Your Online Appointment Booking Details</h4>
+        <p>Booking ID: ${result?.insertedId}</p>
+        <p>Transaction ID: ${booking.transactionId}</p>
+        <p>Thank you for booking with care connect. Your appointment details:</p>
+        <p>Meeting Link will be sent after reviewed by the doctor.</p>
+        <p>We look forward to seeing you at your appointment on time!</p>
+        <p>Best regards,<br>Care Connect Team</p>
+      `;
+
+      sendMail(
+        {
+          subject: "Your Appointment Booked Successfully",
+          message: patientEmailContent,
+        },
+        booking?.guest?.email
+      );
+
+      // Confirmation Email to Doctor
+      const doctorEmailContent = `
+        <h2>Mr X, New Appointment Booked</h2>
+        <p>Booking ID: ${result?.insertedId}</p>
+        <p>Transaction ID: ${booking.transactionId}</p>
+        <p>New appointment booked with you. Please review the details:</p>
+        <p>Meeting Link: <a href="${meetingLink}" target="_blank">${meetingLink}</a></p>
+        <p>Be prepared for the appointment and Confirm it before One hour.</p>
+        <p>Best regards,<br>Care Connect Team</p>
+      `;
+
+      sendMail(
+        {
+          subject: "New Appointment Booked",
+          message: doctorEmailContent,
+        },
+        booking?.host?.email
+      );
+
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Through-Travels Server is running..')
-})
+app.get("/", (req, res) => {
+  res.send("Care Connects Server is running.");
+});
 
 app.listen(port, () => {
-  console.log(`Through-Travels is running on port ${port}`);
-})
+  console.log(`Care Connect is running on port ${port}`);
+});
